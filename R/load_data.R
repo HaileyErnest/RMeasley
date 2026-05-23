0b2718c2d27beb842b829484878400bd2c03224e
Nicole Yee <nicoleeyee8@gmail.com>
2026-05-23 14:42:12 -0700

    Initial commit

---
+++ R/load_data.R
@@ -0,0 +1,10 @@
+#' Load data
+#'
+#' @returns A dataframe of measles data by month
+#' @export
+#'
+#' @examples
+#' load_data()
+load_data <- function(){
+  readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-24/cases_year.csv')
+}
